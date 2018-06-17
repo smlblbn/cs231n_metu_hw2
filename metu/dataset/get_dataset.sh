39ccdc26cef3cd6e5cee426bd85c01bf369ce5eb
smlblbn <smlblbn@gmail.com>
2018-06-17 22:00:12 +0300

    get dataset bash added

--- metu/dataset/get_dataset.sh
+++ metu/dataset/get_dataset.sh
@@ -1,5 +1,5 @@
 # Get BBP dataset Part_1
-#wget https://archive.ics.uci.edu/ml/machine-learning-databases/00340/data.zip
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00340/data.zip
 unzip data.zip
 shopt -s extglob
 rm -r !(Part_1.mat|get_dataset.sh)
